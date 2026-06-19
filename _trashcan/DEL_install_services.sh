e9500e0e30106635f774870036ba68d699f78270
Arne Baier <arnebaier@web.de>
2026-06-19 21:28:44 +0200

    renamed everything to hamlib_rest_api

--- _trashcan/DEL_install_services.sh
+++ _trashcan/DEL_install_services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for id in $(jq -r 'keys[]' /etc/hamlib-rest-api/rigctld.json); do
+for id in $(jq -r 'keys[]' /etc/hamlib_rest_api/rigctld.json); do
     echo "Aktiviere TRX Instanz $id..."
     sudo systemctl daemon-reload
     sudo systemctl enable --now rigctld@$id.service
--- compile.sh
+++ compile.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 go mod init hamlib_rest_api
-GOOS=linux GOARCH=amd64 go build -o build/hamlib-rest-api
-GOOS=linux GOARCH=arm64 go build -o build/hamlib-rest-api
+GOOS=linux GOARCH=amd64 go build -o build/hamlib_rest_api 
+GOOS=linux GOARCH=arm64 go build -o build/hamlib_rest_api
 
-#GOOS=windows GOARCH=amd64 go build -o build/hamlib-rest-api-windows-amd64.exe
-#GOOS=windows GOARCH=arm64 go build -o build/hamlib-rest-api-windows
+#GOOS=windows GOARCH=amd64 go build -o build/hamlib_rest_api-windows-amd64.exe
+#GOOS=windows GOARCH=arm64 go build -o build/hamlib_rest_api-windows
 #arm64.exe
-#GOOS=darwin GOARCH=amd64 go build -o hamlib-rest-api-macos-amd64
-#GOOS=darwin GOARCH=arm64 go build -o hamlib-rest-api-macos-arm64
+#GOOS=darwin GOARCH=amd64 go build -o hamlib_rest_api-macos-amd64
+#GOOS=darwin GOARCH=arm64 go build -o hamlib_rest_api-macos-arm64
 
