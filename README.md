8b7e1e833b4461a08f6f884dd76a044686d297ee
Arne Baier <arnebaier@web.de>
2026-06-19 13:39:24 +0200

    further integration, split insrallation into 3 steps: * run install.sh * modify rigctld.json * run update_rigctld_services.sh

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # hamlib_rest_api
 
-To install use
+To install ***become root*** and use
 
 ```
 cd /tmp
 git clone https://github.com/deinname/hamlib-rest-api.git
 cd hamlib-rest-api
-```
-Modify instances.config according to your needs.
 
-```
-chmod +x install.sh start_rigctld.sh
-sudo ./install.sh
-sudo ./start_rigctld.sh
+# Modify rigctld.config according to your needs:
+editor rigctld.json
+
+chmod +x install.sh update_rigctld_services.sh
+./install.sh
+./update_rigctld_services.sh
 
 ```
--- install.sh
+++ install.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
+set -e
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root (sudo ./install.sh)"
+  exit 1
+fi
 
 apt update && sudo apt install -y libhamlib-utils jq
 
 cp rigctld@.service /etc/systemd/system/
 mkdir -p /etc/hamlib_rest_api
-cp rigctld.json /etc/hamlib_rest_api
-chmod 644 /etc/hamlib_rest_api/rigctld.json
+
+echo "Stopping rigctld services..."
+
+systemctl list-units "rigctld@*" --plain --no-legend
 
 ACTIVE_SERVICES=$(systemctl list-units --all --plain --no-legend "rigctld@*" | awk '{print $1}')
 for service in $ACTIVE_SERVICES; do
-    echo "  -> Stoppe und deaktiviere Dienst: $service"
-    systemctl stop "$service"
-    systemctl disable "$service"
+    if [ -n "$service" ]; then
+        echo "Stopping and deactivating $service ..."
+        systemctl stop "$service" || true
+        systemctl disable "$service" || true
+    fi
 done
 
-#cp hamlib_rest_api.service /etc/systemd/system
-#cp rigctld@.service /etc/systemd/system
+systemctl daemon-reload
 
-systemctl enable --now rigctld@*.service
-# hamlib_rest_api.service
-echo "Please modify /etc/hamlib_rest_api/rigctld.config according to your needs and run update_rigctld_instances.sh"
-
+echo "Please modify rigctld.config according to your needs and run update_rigctld_services.sh"
